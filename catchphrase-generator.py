80d779f27447ddf1cfad6bd07bbc4e3fc3d899d5
Thomas Bredhammar <thomas.bredhammar@gmail.com>
2019-12-06 21:51:35 +0100

    Added catchphrase-generator script

---
+++ catchphrase-generator.py
@@ -0,0 +1,62 @@
+#!/usr/bin/python3
+
+import requests
+import json
+from random import randrange
+
+# https://randomwordgenerator.com/json/
+
+def anything(str_url,key):
+	for url in [str_url]:
+		try:
+			response = requests.get(url)
+			#print(json.loads(response.content))
+			data_dict = json.loads(response.content)
+			#print(data_dict)
+			size = len(data_dict['data'])
+			#print(size)
+			index = randrange(int(size))
+			#print(index)
+			question = data_dict['data'][index][key]
+			print(' ')
+			print(question)
+			print(' ')
+		except:
+			print(' ')
+def phrase():
+	str_url = 'https://randomwordgenerator.com/json/phrases.json'
+	for url in [str_url]:
+		try:
+			response = requests.get(url)
+			#print(json.loads(response.content))
+			data_dict = json.loads(response.content)
+			#print(data_dict)
+			size = len(data_dict['data'])
+			#print(size)
+			index = randrange(int(size))
+			#print(index)
+			phrase = data_dict['data'][index]['phrase']
+			meaning = data_dict['data'][index]['meaning']
+			print(' ')
+			print(phrase)
+			print('Meaning: ' + meaning)
+			print(' ')
+		except:
+			print(' ')
+			
+def main():
+	random = randrange(4)
+	#print(str(random))
+	
+	if random == 1:
+	  anything('https://randomwordgenerator.com/json/questions.json','question')
+	elif random == 2:
+	  anything('https://randomwordgenerator.com/json/facts.json','fact')
+	elif random == 3:
+	  anything('https://randomwordgenerator.com/json/act-of-kindness.json','act_of_kindness')
+	elif random == 4:
+	  anything('https://randomwordgenerator.com/json/act-of-kindness.json','inspirational-quote')
+	else:
+		phrase()
+
+main()
